6981e11fdd7edbaafaebde667b3599a815fbbc61
Khanh Hoang <114916103+vieKH@users.noreply.github.com>
2025-08-27 14:10:10 +0400

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from watermark import payload, encode_watermark, decode_watermark, ber
-
-if __name__ == "__main__":
-    filename = 'wav/samples/test_5.wav'
-    output = 'wav/watermarks/output_5.wav'
-    watermarked_signal = encode_watermark(filename=filename, output=output)
-    payload_decoded = decode_watermark(watermarked_signal=watermarked_signal)
-    ber(payload=payload, payload_decoded=payload_decoded)
